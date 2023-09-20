358f0f505b56534f2fa60c43205961ea9d3c54cd
Alan Sartorio <alan42ga@hotmail.com>
2023-09-20 02:07:26 -0300

    Implement basic map generation

--- franticfortressfrenzy.wavefunctioncollapse/Runtime/WaveFunctionCollapse.cs
+++ franticfortressfrenzy.wavefunctioncollapse/Runtime/WaveFunctionCollapse.cs
@@ -1,6 +1,144 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
-namespace FranticFortressFrenzy.WaveFunctionCollapse {
-    class WaveFunctionCollapse {
+namespace FranticFortressFrenzy.WaveFunctionCollapse
+{
+    public class GridPathGenerator<TC> where TC : IEquatable<TC>
+    {
+        public PathNode<TC> Root { get; }
+        public HashSet<TC> occupied = new();
+        private INeighborGetter<TC> _neighborGetter;
+        private Dictionary<TC, PathNode<TC>> _nodes;
 
+        public GridPathGenerator(INeighborGetter<TC> neighborGetter, TC rootPosition)
+        {
+            _neighborGetter = neighborGetter;
+            Root = new PathNode<TC>(rootPosition, new List<PathNode<TC>>());
+            _nodes = new Dictionary<TC, PathNode<TC>>();
+            _nodes.Add(Root.Position, Root);
+        }
+
+        public void Step()
+        {
+        }
+
+        [CanBeNull]
+        public PathNode<TC> GetNodeInPosition(TC position)
+        {
+            return _nodes[position];
+        }
+
+        private void RegisterNode(PathNode<TC> node)
+        {
+            _nodes.Add(node.Position, node);
+        }
+
+        public void Expand(TC position)
+        {
+            var node = GetNodeInPosition(position);
+            if (node == null)
+                throw new ArgumentException("Position does not exist on path tree");
+
+            var neighs = new HashSet<TC>(_neighborGetter.GetNeighbors(node.Position));
+
+            if (node.Parent != null)
+                neighs.Remove(node.Parent.Position);
+
+            neighs.RemoveWhere(n => _nodes.ContainsKey(n));
+
+            var amount = Random.Range(1, neighs.Count + 1);
+            var neighArray = neighs.ToArray();
+
+            var chosenNeighs = new List<TC>();
+            for (var i = 0; i < amount; i++)
+            {
+                var chosen = Random.Range(i, neighArray.Length);
+                (neighArray[i], neighArray[chosen]) = (neighArray[chosen], neighArray[i]);
+                chosenNeighs.Add(neighArray[i]);
+            }
+
+            foreach (var chosenNeigh in chosenNeighs)
+            {
+                var addedNode = node.AddChild(chosenNeigh);
+                RegisterNode(addedNode);
+            }
+        }
+    }
+
+    public class PathNode<TC> : IEnumerable<PathNode<TC>>
+    {
+        public TC Position { get; }
+        public List<PathNode<TC>> Children { get; }
+        [CanBeNull] public PathNode<TC> Parent { get; }
+
+        public PathNode(TC position, PathNode<TC> parent, List<PathNode<TC>> children)
+        {
+            Position = position;
+            Children = children;
+            Parent = parent;
+        }
+
+        public PathNode(TC position, List<PathNode<TC>> children) : this(position, null, children)
+        {
+        }
+
+        public IEnumerator<PathNode<TC>> GetEnumerator()
+        {
+            return Enumerable.Repeat(this, 1).Concat(Children.SelectMany(c => c)).GetEnumerator();
+        }
+
+        public PathNode<TC> AddChild(TC position)
+        {
+            var node = new PathNode<TC>(position, this, new List<PathNode<TC>>());
+            Children.Add(node);
+            return node;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<PathNode<TC>> Leaves()
+        {
+            return this.Where(n => n.Children.Count == 0);
+        }
+    }
+
+    public interface INeighborGetter<TC>
+    {
+        public IEnumerable<TC> GetNeighbors(TC position);
+    }
+
+    public class Vector2IntNeighborGetter : INeighborGetter<Vector2Int>
+    {
+        public IEnumerable<Vector2Int> GetNeighbors(Vector2Int position)
+        {
+            return new[]
+            {
+                position + Vector2Int.up,
+                position + Vector2Int.down,
+                position + Vector2Int.left,
+                position + Vector2Int.right,
+            };
+        }
+    }
+
+    public class ConstrainedGenerator
+    {
+        public IEnumerable<Vector2Int> CollapsableCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        // public CollapseCell(Vector2Int cell)
+        // {
+        //     throw new NotImplementedException();
+        // }
     }
 }
--- franticfortressfrenzy.wavefunctioncollapse/Tests/Runtime/RuntimeExampleTest.cs
+++ franticfortressfrenzy.wavefunctioncollapse/Tests/Runtime/RuntimeExampleTest.cs
@@ -1,13 +1,70 @@
-
+using System;
+using System.Linq;
+using System.Text;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-namespace FranticFortressFrenzy.WaveFunctionCollapse {
-    class RuntimeExampleTest {
+namespace FranticFortressFrenzy.WaveFunctionCollapse
+{
+    class Tests
+    {
         [Test]
-        public void SimpleTest() {
-            Assert.That(1, Is.EqualTo(2));
+        public void SimpleTest()
+        {
+            var a = new GridPathGenerator<Vector2Int>(new Vector2IntNeighborGetter(), Vector2Int.zero);
+            a.Expand(Vector2Int.zero);
+
+            var expandAnyLeaf = new Action(() =>
+            {
+                var leaves = a.Root.Leaves().ToArray();
+                a.Expand(leaves[Random.Range(0, leaves.Length)].Position);
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                expandAnyLeaf();
+            }
+
+            var map = new char[2 * 10 + 1, 2 * 10 + 1];
+            for (var y = 0; y < map.GetLength(0); y++)
+            {
+                for (var x = 0; x < map.GetLength(1); x++)
+                {
+                    map[y, x] = ' ';
+                }
+            }
+
+            var nodePos = new Func<Vector2Int, Vector2Int>(n => (n + Vector2Int.one * 5) * 2);
+            foreach (var node in a.Root)
+            {
+                var pos = nodePos(node.Position);
+                if (node.Parent == null)
+                {
+                    map[pos.y, pos.x] = 'o';
+                    continue;
+                }
+
+                map[pos.y, pos.x] = '#';
+                var parentPos = nodePos(node.Parent.Position);
+                var connection = (pos + parentPos) / 2;
+                map[connection.y, connection.x] = '*';
+            }
+
+            StringBuilder mapString = new();
+            for (var y = 0; y < map.GetLength(0); y++)
+            {
+                mapString.Append(':');
+                for (var x = 0; x < map.GetLength(1); x++)
+                {
+                    mapString.Append(map[y, x]);
+                }
+
+                mapString.Append('\n');
+            }
+
+            Debug.Log(mapString);
         }
     }
 }
