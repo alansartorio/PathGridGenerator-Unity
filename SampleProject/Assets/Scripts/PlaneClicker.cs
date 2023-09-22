106496a0ba9e8fafefdfee87c92c30858af98b00
Alan Sartorio <alan42ga@hotmail.com>
2023-09-22 16:54:55 -0300

    Add scroll to zoom camera in sample

--- SampleProject/Assets/Scripts/PlaneClicker.cs
+++ SampleProject/Assets/Scripts/PlaneClicker.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlaneClicker : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler
+public class PlaneClicker : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IScrollHandler
 {
     private Generator _generatorObject;
 
@@ -27,11 +27,20 @@ public class PlaneClicker : MonoBehaviour, IPointerClickHandler, IDragHandler, I
         Vector2 delta = position - dragStart;
         var cam = Camera.main;
         dragStart += delta;
-        cam.transform.position -= new Vector3(delta.x, 0, delta.y) * 0.015f;
+        cam.transform.position -= new Vector3(delta.x, 0, delta.y) * 0.0015f * cam.transform.position.y;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         dragStart = eventData.position;
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        var newPos = Camera.main.transform.position;
+        newPos.y -= eventData.scrollDelta.y;
+        if (newPos.y < 1)
+            newPos.y = 1;
+        Camera.main.transform.position = newPos;
     }
 }
