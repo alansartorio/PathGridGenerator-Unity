c214cfeb50fdcccfecee4e2cc112529d25a82a1d
Alan Sartorio <alan42ga@hotmail.com>
2023-09-22 16:28:06 -0300

    Add drag to move camera

--- SampleProject/Assets/Scripts/PlaneClicker.cs
+++ SampleProject/Assets/Scripts/PlaneClicker.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlaneClicker : MonoBehaviour, IPointerClickHandler
+public class PlaneClicker : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler
 {
     private Generator _generatorObject;
 
@@ -17,5 +17,21 @@ public class PlaneClicker : MonoBehaviour, IPointerClickHandler
         var pos = new Vector2Int((int)Math.Round(hitPos.x), (int)Math.Round(hitPos.z));
 
         _generatorObject.EnableNodeExpand(pos);
+    }
+
+    private Vector2 dragStart;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var position = eventData.position;
+        Vector2 delta = position - dragStart;
+        var cam = Camera.main;
+        dragStart += delta;
+        cam.transform.position -= new Vector3(delta.x, 0, delta.y) * 0.015f;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragStart = eventData.position;
     }
 }
